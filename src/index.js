0998773afaefe246fcd8f67b9bcc240907cd3176
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-01-23 18:46:24 +0300

    feat: add useState

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <HookSwitcher/>
+  )
+}
 
+const HookSwitcher = () => {
+  const [color, setColor] = useState('black')
+  const [fontSize, setFontSize] = useState(14)
+  return (
+    <div style={{ backgroundColor: color, padding: "10px", fontSize: `${fontSize}px` }}>
+      Hello world!
+      <button onClick={() => setColor('black')}>Dark</button>
+      <button onClick={() => setColor('white')}>Light</button>
+      <button onClick={() => setFontSize((size) => size + 2)}>+2px</button>
+    </div>
+  )
+}
 
-ReactDOM.render(<h1>Hi</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
